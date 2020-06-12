ecdb125ea3e55015eb809f7b3eada381f731a328
User <user@Users-MacBook-Air-2.local>
2020-06-13 09:50:19 +1200

    image boxes

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var temp = document.getElementsByTagName("template")[0];
+var clon = temp.content.cloneNode(true);
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+var i;
+var shuff = [];
+for (i = 0; i < projects.length; i++){
+  shuff[i] = i;
+}
+
+shuff = shuffle(shuff);
+console.log(shuff)
+
+for (i = 0; i < projects.length; i++) {
+  clon.getElementById("title").innerHTML = projects[shuff[i]]['title'];
+  clon.getElementById("description").innerHTML = projects[shuff[i]]['description'];
+  clon.getElementById("url").href= projects[shuff[i]]['url']
+  clon.getElementById("src").src= projects[shuff[i]]['src']
+  document.body.appendChild(clon);
+  clon = temp.content.cloneNode(true);
+}
